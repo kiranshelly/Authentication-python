4bf31687bab10145e589ab7bb4b51839567e6f32
kiranshelly <48305818+kiranshelly@users.noreply.github.com>
2021-10-06 15:35:23 +0530

    Changes

--- First/twilio_fun.py
+++ First/twilio_fun.py
@@ -1,15 +1,15 @@
 
 from twilio.rest import Client
 
-ACCOUNT_SID = 'AC4e6b71910edc727de5a4b32e82ddd527'
-AUTH_TOKEN = 'cc5d59a41783f8306f4e6125fb43f946'
+ACCOUNT_SID = Your Number
+AUTH_TOKEN = Your Number
 
 c = Client(ACCOUNT_SID,AUTH_TOKEN)
 
 m = c.messages.create(
     body="Twilio Here",
-    from_= "+19363427869" ,
-    to= "+917837040632"
+    from_= Your Twilio Number,
+    to= Your NUmber
 )
 print(m)
 
